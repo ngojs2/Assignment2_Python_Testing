910cb06aff0409ef4bbbf959e0eee91c9553588c
Jeff Ngo <ngojef@oregonstate.edu>
2020-07-27 13:35:53 -0700

    Wrote test1 to see if a short password fails

--- tests.py
+++ tests.py
@@ -4,6 +4,11 @@ from check_pwd import check_pwd
 
 class TestCase(unittest.TestCase):
 
+    # Tests password length
+    def test1(self):
+        password = 'aBc23@'
+        self.assertFalse(check_pwd(password))
+
 
 if __name__ == '__main__':
     unittest.main()
