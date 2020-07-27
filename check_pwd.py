7acf8a3a0e0b5b297fd4296360b19da0a5b89bc0
Jeff Ngo <ngojef@oregonstate.edu>
2020-07-27 13:53:41 -0700

    Made check_pwd return False if no lowercase in pw

--- check_pwd.py
+++ check_pwd.py
@@ -7,4 +7,9 @@ def check_pwd(password):
     if not any(char.isupper() for char in password):
         return False
 
+    if not any(char.islower() for char in password):
+        return False
+
+
+
     return True
