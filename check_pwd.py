69c25f04f8de6a1baeda1933868954a4df7f3050
Jeff Ngo <ngojef@oregonstate.edu>
2020-07-27 13:46:31 -0700

    Made check_pwd return False if no uppercase in pw

--- check_pwd.py
+++ check_pwd.py
@@ -4,4 +4,7 @@ def check_pwd(password):
     if len(password) < 8 or len(password) > 20:
         return False
 
+    if not any(char.isupper() for char in password):
+        return False
+
     return True
